a7220c18b4f74ff711e3c0ee0b25a42fb7b86ba5
Rozen Deedi <74816055+Rozen2007@users.noreply.github.com>
2021-05-24 02:55:32 +0530

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -25,8 +25,8 @@ export default async (req, res) => {
         },
         line_items: transformedItems,
         mode: "payment",
-        success_url: "https://amazon-clone-2.vercel.app/success",
-        cancel_url: "https://amazon-clone-2.vercel.app/basket",
+        success_url: "https://amazon-clone-2-ochre.vercel.app/success",
+        cancel_url: "https://amazon-clone-2-ochre.vercel.app/basket",
         metadata: {
             email, 
             images: JSON.stringify(items.map(item => item.image)),
