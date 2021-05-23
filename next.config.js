8c15b12edcafcf14034adf949322fb521bc7a322
Rozen Deedi <74816055+Rozen2007@users.noreply.github.com>
2021-05-24 02:44:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
         domains: ['links.papareact.com', 'fakestoreapi.com']
     },
     env: {
-        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+        stripe_public_key: 'pk_test_51IuM8vSCwLBSaM61Yb6bCHjsJGZxttJI60epJAQkQ3fIEIrI81FOvfRHBYboRX9bqnshsXR1GklypzhxYuY85kP700FD3pDm2f'
     }
 }
